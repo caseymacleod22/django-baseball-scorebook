815f32e25778e51bf253d94d3fe98a8c8a7be483
Casey MacLeod <casey@Caseys-MacBook-Pro.local>
2021-05-07 12:17:44 -0400

    display all teams, adds basic scoreboard

--- scorebook/models.py
+++ scorebook/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
 class Team(models.Model):
     location = models.CharField(max_length=50)
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location
--- scorebook/views.py
+++ scorebook/views.py
@@ -8,4 +8,6 @@ def index(request):
     return render(request, 'index.html')
     
 def teams(request):
-    return render(request, 'teams.html')
+    return render(request, 'teams.html', {
+        'scorebook': Team.objects.all()
+    })
